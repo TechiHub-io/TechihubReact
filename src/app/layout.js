6dccf393a6c49d7a0ce31ceb49cb4fad38d6d02e
svitalis123 <vitalis.wilson@yahoo.com>
2025-06-28 14:12:46 +0300

    added font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,13 @@
 // src/app/layout.js
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  display: 'swap',
+});
+
 
 export const metadata = {
   title: "TechHub - Your Gateway to Professional Opportunities",
@@ -13,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         {children}
       </body>
     </html>
